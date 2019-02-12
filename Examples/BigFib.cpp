d8f60eb920f2683d9696fadf070ba1fe6bf2ef9e
cthelight <46094900+cthelight@users.noreply.github.com>
2019-02-12 15:44:15 -0600

    Improved fibonacci algorithm

--- Examples/BigFib.cpp
+++ Examples/BigFib.cpp
@@ -1,10 +1,13 @@
 #include "../BigNum.h"
 #include <time.h>
+#include <math.h>
 
 BigNum getFib(int num);
 
-int main(int argc, char *argv[]) {
-	if (argc > 1) {
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
 		clock_t start = clock();
 		int i = atoi(argv[1]);
 
@@ -13,22 +16,43 @@ int main(int argc, char *argv[]) {
 		clock_t end = clock();
 		std::cout << "Time to complete: " << (double)(end - start) / CLOCKS_PER_SEC << " s" << std::endl;
 	}
-	else {
+	else
+	{
 		std::cout << "Enter the Fibonacci to generate as an argument. Aborting..." << std::endl;
 	}
 	return 0;
 }
 
-BigNum getFib(int num) {
-	BigNum a(1);
-	BigNum b(1);
+BigNum getFib(int num)
+{
+	char *steps = (char *)malloc((int)ceil(log2(num) + 1) * sizeof(char));
+	int cur = num;
+	int count;
+	for (count = 0; cur > 1; count++)
+	{
+		steps[count] = cur % 2;
+		cur = cur / 2;
+	}
+	BigNum k_val = 1;
+	BigNum k1_val = 1;
+	BigNum k_val_n = 1;
+	BigNum k1_val_n = 1;
 	BigNum temp(0);
 
 	int i;
-	for (i = 0; i < num - 2; i++) {
-		temp = a + b;
-		b = a;
-		a = temp;
+	for (i = count - 1; i >= 0; i--)
+	{
+		k_val_n = k_val * ((2 * k1_val) - k_val);
+		k1_val_n = (k1_val * k1_val) + (k_val * k_val);
+		k_val = k_val_n;
+		k1_val = k1_val_n;
+		if (steps[i])
+		{
+			temp = k_val + k1_val;
+			k_val = k1_val;
+			k1_val = temp;
+		}
 	}
-	return a;
+	free(steps);
+	return k_val;
 }
