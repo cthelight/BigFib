bafc1092d2c55db0debb31378b6eb0b882e4dea8
cthelight <46094900+cthelight@users.noreply.github.com>
2019-02-15 17:52:54 -0600

    Initial threaded implementation

--- Examples/BigFib.cpp
+++ Examples/BigFib.cpp
@@ -1,8 +1,21 @@
 #include "../BigNum.h"
 #include <time.h>
 #include <math.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 BigNum getFib(int num);
+void *change_k1_val(void *unused);
+void *change_k_val(void *unused);
+
+pthread_t threads[2];
+sem_t sems[2];
+sem_t sem_main;
+
+BigNum k_val = 1;
+BigNum k1_val = 1;
+BigNum k_val_n = 1;
+BigNum k1_val_n = 1;
 
 int main(int argc, char *argv[])
 {
@@ -10,7 +23,11 @@ int main(int argc, char *argv[])
 	{
 		clock_t start = clock();
 		int i = atoi(argv[1]);
-
+		sem_init(&sem_main, 0, 0);
+		sem_init(&sems[0], 0, 0);
+		sem_init(&sems[1], 0, 0);
+		pthread_create(&threads[0], NULL, change_k_val, NULL);
+		pthread_create(&threads[1], NULL, change_k1_val, NULL);
 		BigNum fib = getFib(i);
 		fib.print();
 
@@ -35,19 +52,24 @@ BigNum getFib(int num)
 		steps[count] = cur % 2;
 		cur = cur / 2;
 	}
-	BigNum k_val = 1;
-	BigNum k1_val = 1;
-	BigNum k_val_n = 1;
-	BigNum k1_val_n = 1;
+	
 	BigNum temp(0);
 
 	int i;
 	for (i = count - 1; i >= 0; i--)
 	{
-		k_val_n = k_val * ((2 * k1_val) - k_val);
-		k1_val_n = (k1_val * k1_val) + (k_val * k_val);
-		k_val = k_val_n;
-		k1_val = k1_val_n;
+		// k_val_n = k_val * ((2 * k1_val) - k_val);
+		// k1_val_n = (k1_val * k1_val) + (k_val * k_val);
+		// k_val = k_val_n;
+		// k1_val = k1_val_n;
+		sem_post(&sems[0]);
+		sem_post(&sems[1]);
+		sem_wait(&sem_main);
+		sem_wait(&sem_main);
+		sem_post(&sems[0]);
+		sem_post(&sems[1]);
+		sem_wait(&sem_main);
+		sem_wait(&sem_main);
 		if (steps[i])
 		{
 			temp = k_val + k1_val;
@@ -58,3 +80,28 @@ BigNum getFib(int num)
 	free(steps);
 	return k_val;
 }
+
+void *change_k_val(void *unused){
+	while(true){
+		sem_wait(&sems[0]);
+		k_val_n = k_val * ((2 * k1_val) - k_val);
+		sem_post(&sem_main);
+		sem_wait(&sems[0]);
+		k_val = k_val_n;
+		sem_post(&sem_main);
+	}
+	return NULL;
+}
+
+void *change_k1_val(void *unused){
+	while(true){
+		sem_wait(&sems[1]);
+		k1_val_n = (k1_val * k1_val) + (k_val * k_val);
+		sem_post(&sem_main);
+		sem_wait(&sems[1]);
+		k1_val = k1_val_n;
+		sem_post(&sem_main);
+	}
+	return NULL;
+}
+
